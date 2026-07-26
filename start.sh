18f1dfefc1a6d43939a388c7a8191811d569177a
Yuv-12 <yuvrajgupta1209@gmail.com>
2026-07-26 16:52:05 +0530

    Update start.sh and apply LF format

--- start.sh
+++ start.sh
@@ -9,3 +9,5 @@ sleep 2
 # Deploy the C++ Load Balancer, binding it to the PORT environment variable provided by Render (defaults to 10000)
 echo "Starting C++ Load Balancer on port ${PORT:-10000}..."
 ./cpp/simplelb --backends http://127.0.0.1:3031,http://127.0.0.1:3032,http://127.0.0.1:3033,http://127.0.0.1:3034 --port ${PORT:-10000}
+
+# Enforce Unix LF line ending verification
